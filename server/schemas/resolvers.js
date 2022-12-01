02fc842a6a413c1577a30dfaaa5272ec555d96de
Kamnaah <kamnasao45@gmail.com>
2022-12-01 19:59:26 +0530

    added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,64 @@
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
+const resolvers = {
+  Query: {
+    //user resolver
+    users: () => {
+      return UserList;
+    },
+    user: (parents, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+    //movie resolver
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parents, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name: name });
+      return movie;
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
+    },
+  },
+  //mutation : put , post , delete
+
+  Mutation: {
+    createUser: (parents, args) => {
+      const user = args.input;
+      const lastId = UserList[UserList.length - 1].id;
+      user.id = lastId + 1;
+      UserList.push(user);
+      return user;
+    },
+
+    updateUserName: (parents, args) => {
+      const { id, newName } = args.input;
+      let userUpdated;
+      UserList.forEach((user) => {
+        if (user.id == id) {
+          user.name = newName;
+          userUpdated = user;
+        }
+      });
+      return userUpdated;
+    },
+
+    deleteUser: (parents, args) => {
+      const id = args.id;
+      _.remove(UserList, (user) => user.id === Number(id));
+      return null;
+    },
+  },
+};
+
+module.exports = { resolvers };
